44c6e48ff47d091049a16caed75c3e341acb3dc0
kvmlt <kvmltkv@gmail.com>
2023-12-25 20:57:07 +0200

    Added class User and fixture user to file conftest.py

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+class User:
+    def __init__(self) -> None:
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = "Vadym"
+        self.second_name = "Kosnovskyi"
+
+    def remove(self):
+        self.name = ""
+        self.second_name = ""
+
+
+@pytest.fixture()
+def user():
+    user = User()
+    yield user.create()
+    user.remove()
